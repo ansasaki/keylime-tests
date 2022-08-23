314e4d30318b0767f3b67979c408839887406351
Patrik Koncity <pkoncity@redhat.com>
2022-08-23 11:46:59 +0200

    Add revocation script which run sudo cmd

--- functional/basic-attestation-on-localhost/test.sh
+++ functional/basic-attestation-on-localhost/test.sh
@@ -15,6 +15,8 @@ rlJournalStart
 
         # update /etc/keylime.conf
         limeBackupConfig
+        rlFileBackup --clean --missing-ok /etc/sudoers
+        rlRun "echo 'keylime ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
         # verifier
         rlRun "limeUpdateConf cloud_verifier revocation_notifiers ${REVOCATION_NOTIFIER},webhook"
         rlRun "limeUpdateConf cloud_verifier webhook_url http://localhost:${HTTP_SERVER_PORT}"
@@ -96,6 +98,7 @@ _EOF"
             cat ${HTTP_SERVER_LOG}
             rlAssertGrep '\\"type\\": \\"revocation\\", \\"ip\\": \\"127.0.0.1\\", \\"agent_id\\": \\"d432fbb3-d2f1-4a97-9ef7-75bd81c00000\\"' ${HTTP_SERVER_LOG} -i
             rlAssertNotGrep ERROR ${HTTP_SERVER_LOG} -i
+            rlAssertGrep 'root' /var/lib/keylime/sudo_script_output.txt -i
         fi
     rlPhaseEnd
 
@@ -103,6 +106,7 @@ _EOF"
         rlRun "kill ${HTTP_SERVER_PID}"
         rlRun "rm ${HTTP_SERVER_LOG}"
         rlRun "rm -f /var/tmp/test_payload_file"
+        rlRun "rm -f /var/lib/keylime/sudo_script_output.txt"
         rlRun "limeStopAgent"
         rlRun "limeStopRegistrar"
         rlRun "limeStopVerifier"
@@ -118,6 +122,7 @@ _EOF"
         limeClearData
         limeRestoreConfig
         limeExtendNextExcludelist $TESTDIR
+        rlFileRestore
         #rlRun "rm -f $TESTDIR/keylime-bad-script.sh"  # possible but not really necessary
     rlPhaseEnd
 
