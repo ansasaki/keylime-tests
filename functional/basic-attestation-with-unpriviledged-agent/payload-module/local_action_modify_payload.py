45cad4f6bd2c1df540d7564fa6ebfe5e9827f089
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2023-07-19 14:52:59 +0200

    Remove usage of keylime.config from revocation actions payloads
    Since commit 12aae6b [1], the python keylime config module does not load
    the agent configuration.
    
    The revocation actions used as python modules (legacy revocation
    actions) relies on the agent configuration to be loaded by the python
    keylime config module.
    
    [1] https://github.com/keylime/keylime/commit/12aae6b8500a9a12a1649f65ff17333f1fbe190b
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- functional/basic-attestation-with-unpriviledged-agent/payload-module/local_action_modify_payload.py
+++ functional/basic-attestation-with-unpriviledged-agent/payload-module/local_action_modify_payload.py
@@ -1,21 +1,18 @@
 import os
-import ast
-import keylime.secure_mount as secure_mount
-from keylime import keylime_logging
-from keylime import ca_util
-from keylime import config
-
-logger = keylime_logging.init_logging("local_action_rm_ssh")
+import toml
 
 async def execute(event):
     if event.get("type") != "revocation":
         return
 
-    # load up my own cert
     event_uuid = event.get("agent_id", "my")
-    my_uuid = config.get('agent', 'uuid')
-    logger.info("A node in the network has been compromised: %s", event["ip"])
-    logger.info("my UUID: %s, event UUID: %s" % (my_uuid, event_uuid))
+    event_ip = event.get("event_ip", "my")
+
+    with open("/etc/keylime/agent.conf", "r") as f:
+        my_uuid = toml.load(f)["agent"]["uuid"].strip('\"')
+        print("A node in the network has been compromised:", event_ip)
+        print("my UUID: %s, event UUID: %s" % (my_uuid, event_uuid))
+
     # is this revocation meant for me?
     if my_uuid == event_uuid:
         os.remove("/var/tmp/test_payload_file")
